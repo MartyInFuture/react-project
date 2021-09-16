e98fbee4fc203ee2a1c06f552fa05613fd4fdefb
Oleksandr2891 <allavrenko@gmail.com>
2021-09-16 22:18:46 +0300

    last change in modal

--- src/Components/common/modal/Modal.jsx
+++ src/Components/common/modal/Modal.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { Overlay } from "./ModalStyled";
 
+//для работы модалки необходимо передать 3 пропса,
+// в первом передаем метод который будет переключать в стейте флаг
+// вторым пропсом передаем компонент формы с кнопкой сабмит
+// третьим передаем имя формы
+
 const Modal = ({ closeModal, children, nameForm = "Name form" }) => {
   useEffect(() => {
     window.addEventListener("keydown", handleEscape);
@@ -21,9 +26,12 @@ const Modal = ({ closeModal, children, nameForm = "Name form" }) => {
   return (
     <Overlay onClick={onOverlayClick}>
       <div className="modal">
+        <button type="button" className="closeBtn" onClick={closeModal}>
+          <span className="material-icons-outlined">close</span>
+        </button>
         <h2 className="nameForm">{nameForm}</h2>
         {children}
-        <button type="button" className="cancel">
+        <button type="button" className="cancelBtn" onClick={closeModal}>
           Відміна
         </button>
       </div>
