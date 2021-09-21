4cc6c490caad3032a8de4f3932d9f6d9e4ab4f37
unknown <martyinfuture@gmail.com>
2021-09-21 18:08:46 +0300

    fix

--- src/Components/projects/addMembers/MembersForm.jsx
+++ src/Components/projects/addMembers/MembersForm.jsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { addMember } from "../../../redux/addMembers/addMembers-operations"
-import SubmitButton from "../../common/submitButton/SubmitButton"
-import { WrapperForm } from "./MembersFormStyled"
-import MembersList from "./MembersList"
-import { useParams } from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { addMember } from "../../../redux/addMembers/addMembers-operations"
+import SubmitButton from '../../common/submitButton/SubmitButton';
+import { WrapperForm } from './MembersFormStyled';
+import MembersList from './MembersList';
+import { useParams } from 'react-router-dom';
+import projectOperations from '../../../redux/projects/projects-operations';
 
 const MembersForm = ({ setOpenModal }) => {
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState('');
 
-  const { id } = useParams()
+  const { id } = useParams();
 
   const handleChange = (e) => {
-    setEmail(e.currentTarget.value)
-  }
+    setEmail(e.currentTarget.value);
+  };
 
   const members = useSelector((state) => {
-    return state.projects.filter((project) => project._id === id)[0].members
-  })
+    return state.projects.filter((project) => project._id === id)[0].members;
+  });
+  useEffect(() => {
+    console.log('hello');
+  }, [members]);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const onHandleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(addMember({ id, email: { email } }))
-    setEmail("")
-    setOpenModal(false)
-  }
+    e.preventDefault();
+    dispatch(projectOperations.addMember({ id, email: { email } }));
+    setEmail('');
+    setOpenModal(false);
+  };
 
   return (
     <WrapperForm>
@@ -53,7 +57,7 @@ const MembersForm = ({ setOpenModal }) => {
         </div>
       </form>
     </WrapperForm>
-  )
-}
+  );
+};
 
-export default MembersForm
+export default MembersForm;
