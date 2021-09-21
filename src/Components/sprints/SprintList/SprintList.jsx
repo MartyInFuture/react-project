c0dc140cbcd975c5f060be163fd1b03c5a519a19
Oleksandr2891 <allavrenko@gmail.com>
2021-09-21 11:05:44 +0300

    fetch all sprints and logik post

--- src/Components/sprints/SprintList/SprintList.jsx
+++ src/Components/sprints/SprintList/SprintList.jsx
@@ -1,65 +1,63 @@
 /* eslint-disable no-undef */
 // import { useSelector } from 'react-redux';
 
-import SprintListItem from '../SprintListItem/SprintListItem';
-import { SprintListStyled } from './SprintListStyled';
+import SprintListItem from "../SprintListItem/SprintListItem";
+import { SprintListStyled } from "./SprintListStyled";
 
-const SprintList = () => {
+const SprintList = ({ sprints }) => {
+  // const data = {
+  //   sprints: [
+  //     {
+  //       project: 1,
+  //       name: "Sprint 1",
+  //       startDate: "2020-12-30",
+  //       endDate: "2020-12-31",
+  //       duration: 100,
+  //       id: 243424,
+  //     },
+  //     {
+  //       project: 1,
+  //       name: "Sprint 2",
+  //       startDate: "2020-12-30",
+  //       endDate: "2021-12-31",
+  //       duration: 400,
+  //       id: 243425,
+  //     },
+  //     {
+  //       project: 1,
+  //       name: "Sprint 3",
+  //       startDate: "2020-12-30",
+  //       endDate: "2021-12-31",
+  //       duration: 300,
+  //       id: 243426,
+  //     },
+  //     {
+  //       project: 1,
+  //       name: "Sprint 4",
+  //       startDate: "2020-12-30",
+  //       endDate: "2021-12-31",
+  //       duration: 4,
+  //       id: 243427,
+  //     },
+  //   ],
+  // };
 
-  const data = {
-    sprints: [
-      {
-        project: 1,
-        name: 'Sprint 1',
-        startDate: '2020-12-30',
-        endDate: '2020-12-31',
-        duration: 100,
-        id: 243424
-        },
-      {
-        project: 1,
-        name: 'Sprint 2',
-        startDate: '2020-12-30',
-        endDate: '2021-12-31',
-        duration: 400,
-        id: 243425
-      },
-      {
-        project: 1,
-        name: 'Sprint 3',
-        startDate: '2020-12-30',
-        endDate: '2021-12-31',
-        duration: 300,
-        id: 243426
-      },
-      {
-        project: 1,
-        name: 'Sprint 4',
-        startDate: '2020-12-30',
-        endDate: '2021-12-31',
-        duration: 4,
-        id: 243427
-      }
-    ]
-  };
-  
   return (
     <>
       <SprintListStyled>
-        
         <p className="listWrapper">Create first sprint</p>
-       
-          <ul className="listWrapper">
-            {data.sprints.map(sprint => (
+
+        <ul className="listWrapper">
+          {sprints &&
+            sprints.map((sprint) => (
               <li key={sprint.id} className="listItem">
-                <SprintListItem sprint={sprint}/>
+                <SprintListItem sprint={sprint} />
               </li>
             ))}
-          </ul>   
-       
+        </ul>
       </SprintListStyled>
     </>
-  );  
+  );
 };
 
 export default SprintList;
--- src/pages/sprints/Sprint.jsx
+++ src/pages/sprints/Sprint.jsx
@@ -1,28 +1,133 @@
-import { useDispatch } from "react-redux";
 import Button from "../../Components/common/button/Button";
-import { addSprint } from "../../redux/sprints/sprints-operations";
+import Title from "../../Components/common/title/Title";
+import SprintList from "../../Components/sprints/SprintList/SprintList";
+// import CreateMembers from '../Components/projects/addMembers/CreateMembers';
+import { SprintStyled } from "./SprintStyled";
+import buttonIcons from "../../configs/buttonIcons.json";
+import NavContainer from "../../Components/common/containers/navContainer/NavContainer";
+import NavMenu from "../../Components/navMenu/NavMenu";
+// import CreateMembers from "../Components/projects/addMembers/CreateMembers"
+import CreateMembers from "../../Components/projects/addMembers/createMembers";
+import { useState, useEffect } from "react";
+import CreateSprint from "../../Components/sprints/createSprint/CreateSprint";
+import { useDispatch, useSelector } from "react-redux";
+import { token } from "../../redux/auth/auth-operations";
+import { authSelectors } from "../../redux/auth";
+import { getProjectsSprints } from "../../redux/sprints/sprints-operations";
+import { useHistory } from "react-router";
+import sprintSelectors from "../../redux/sprints/sprints-selectors";
 
-const Sprint = () => {
+const SprintPage = () => {
+  const [openModalMembers, setOpenModalMembers] = useState(false);
+  const [openModalSprints, setOpenModalSprints] = useState(false);
+  const isAuth = useSelector(authSelectors.getAccessToken);
+  const sprints = useSelector(sprintSelectors.getSprints);
   const dispatch = useDispatch();
-  const onHandleClick = () => {
-    const projectId = "614776eef4a6c03db8cc8ef3";
-    dispatch(
-      addSprint({
-        projectId,
-        sprintData: {
-          title: "Sprint 6",
-          endDate: "2020-12-31",
-          duration: 1,
-        },
-      })
-    );
-  };
+  const history = useHistory();
+  const idProject = history.location.pathname.slice(9);
+
+  // const onHandleClick = () => {
+  //   const projectId = "614776eef4a6c03db8cc8ef3";
+  //   dispatch(
+  //     addSprint({
+  //       projectId,
+  //       sprintData: {
+  //         title: "Sprint 6",
+  //         endDate: "2020-12-31",
+  //         duration: 1,
+  //       },
+  //     })
+  //   );
+  // };
+
+  useEffect(() => {
+    token.set(isAuth);
+    isAuth && dispatch(getProjectsSprints(idProject));
+  }, [dispatch]);
+
   return (
     <>
-      <Button onHandleClick={onHandleClick} />
-      <h1>Это спринты</h1>
+      <NavContainer>
+        <NavMenu />
+      </NavContainer>
+      <SprintStyled>
+        {/* <div className="main"> */}
+        <article>
+          <div className="headerWrap">
+            <div className="contentWrap">
+              <div className="titleWrap">
+                <form>
+                  <input
+                    className="inputChangeTitle"
+                    // value={name}
+                    name="name"
+                    type="text"
+                    // onChange={handleNameChange}
+                  />
+                  {/* <button
+                                    onHandleClick={null}
+                                    
+                                /> */}
+                </form>
+                {/* {!showInput && ( */}
+                <>
+                  <Button
+                    icon={buttonIcons.edit}
+                    classBtn="editDelete"
+                    title="Edit the name"
+                    type="button"
+                    className="buttonChange"
+                    // onClick={editNameHandle}
+                  />
+                  <Title />
+                </>
+                {/* )} */}
+              </div>
+
+              <p>current project description</p>
+
+              <div className="addWrap">
+                <button
+                  className="btnWrap"
+                  onClick={() => setOpenModalMembers(true)}
+                >
+                  <Button
+                    icon={buttonIcons.group_add}
+                    classBtn="group_add"
+                    title="Add people"
+                    type="button"
+                  />
+                  <span className="textAddPeople">Add people</span>
+                </button>
+
+                <CreateMembers
+                  closeModal={openModalMembers}
+                  setCloseModal={setOpenModalMembers}
+                />
+              </div>
+            </div>
+
+            {767 && (
+              <>
+                <Button
+                  icon={buttonIcons.add}
+                  classBtn="add"
+                  onHandleClick={() => setOpenModalSprints(true)}
+                />
+                <CreateSprint
+                  closeModal={openModalSprints}
+                  setCloseModal={setOpenModalSprints}
+                />
+              </>
+            )}
+          </div>
+
+          <SprintList sprints={sprints} />
+        </article>
+        {/* </div> */}
+      </SprintStyled>
     </>
   );
 };
 
-export default Sprint;
+export default SprintPage;
