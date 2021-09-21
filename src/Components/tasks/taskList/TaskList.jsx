a94ff2036d821c6607e741af936ec3ae10bc0dcf
PetiurAliona <alena.petyur@gmail.com>
2021-09-21 23:26:32 +0300

    +taskList

--- src/Components/tasks/taskList/TaskList.jsx
+++ src/Components/tasks/taskList/TaskList.jsx
@@ -1,36 +1,34 @@
-import TaskListItem from "../taskListItem/TaskListItem";
-import { useState, useEffect } from "react";
-import { Tasklist } from "./TaskListStyled";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { getSprintsTasks } from "../../../redux/task/task-operations";
-import { useParams } from "react-router-dom";
-import taskSelectors from "../../../redux/task/task-selectors";
+import TaskListItem from "../taskListItem/TaskListItem"
+import { useState, useEffect } from "react"
+import { Tasklist } from "./TaskListStyled"
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { getSprintsTasks } from "../../../redux/task/task-operations"
+import { useParams } from "react-router-dom"
+import taskSelectors from "../../../redux/task/task-selectors"
 
 const TaskList = ({ filter }) => {
-  const tasks = useSelector(taskSelectors.getTasks);
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  const [filtredTasks, setFiltredTasks] = useState([]);
+  const tasks = useSelector(taskSelectors.getTasks)
+  const dispatch = useDispatch()
+  const { id } = useParams()
+  const [filtredTasks, setFiltredTasks] = useState([])
 
   useEffect(() => {
-    dispatch(getSprintsTasks(id));
+    dispatch(getSprintsTasks(id))
 
-//   }, []);
+    //   }, []);
 
-//   useEffect(() => {
-//     if (tasks) {
-//       const res = tasks.filter((task) => task.title.includes(filter));
-//       setFiltredTasks(res);
-//     }
-//   }, [filter, tasks]);
-
-  }, [id]);
+    //   useEffect(() => {
+    //     if (tasks) {
+    //       const res = tasks.filter((task) => task.title.includes(filter));
+    //       setFiltredTasks(res);
+    //     }
+    //   }, [filter, tasks]);
+  }, [id])
   useEffect(() => {
-    const res = tasks.filter((task) => tasks.title?.includes(filter));
-    setFiltredTasks(res);
-  }, [filter]);
-
+    const res = tasks.filter((task) => tasks.title?.includes(filter))
+    setFiltredTasks(res)
+  }, [filter])
 
   //   const filterTasks = () => {
   //     const res = tasks.filter((task) => tasks.title.includes(Filter));
@@ -38,20 +36,16 @@ const TaskList = ({ filter }) => {
   //   };
 
   return (
-    <Tasklist>
-
-      {filtredTasks.length === 0 &&
-        tasks.map((item) => <TaskListItem key={item._id} task={item} />)}
-      {filtredTasks.length > 0 &&
-        filtredTasks.map((item) => <TaskListItem key={item._id} task={item} />)}
-
+    <TaskList>
+      {filtredTasks.length === 0 && tasks.map((item) => <TaskListItem key={item._id} task={item} />)}
+      {filtredTasks.length > 0 && filtredTasks.map((item) => <TaskListItem key={item._id} task={item} />)}
       {/* filterText === "" && */}
-//       {tasks?.map((item) => (
-//         <TaskListItem key={item._id} task={item} />
-//       ))}
+      //{" "}
+      {tasks?.map((item) => (
+        <TaskListItem key={item._id} task={item} />
+      ))}
+    </TaskList>
+  )
+}
 
-    </Tasklist>
-  );
-};
-
-export default TaskList;
+export default TaskList
