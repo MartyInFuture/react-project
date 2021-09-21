ab7879628d4c7ef7912c1ef2ba64f8faa6601d00
TheDeveloperAlex <scorpion422@ukr.net>
2021-09-21 14:13:00 +0300

    Added filter

--- src/Components/tasks/taskList/TaskList.jsx
+++ src/Components/tasks/taskList/TaskList.jsx
@@ -1,19 +1,30 @@
-import TaskListItem from '../taskListItem/TaskListItem';
-import { useState, useEffect } from 'react';
-import { Tasklist } from './TaskListStyled';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { getSprintsTasks } from '../../../redux/task/task-operations';
-import { useParams } from 'react-router-dom';
+import TaskListItem from "../taskListItem/TaskListItem";
+import { useState, useEffect } from "react";
+import { Tasklist } from "./TaskListStyled";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { getSprintsTasks } from "../../../redux/task/task-operations";
+import { useParams } from "react-router-dom";
+import taskSelectors from "../../../redux/task/task-selectors";
 
-const TaskList = ({ filtredTasks }) => {
-  const tasks = useSelector((state) => state.tasks.items);
+const TaskList = ({ filter }) => {
+  const tasks = useSelector(taskSelectors.getTasks);
   const dispatch = useDispatch();
   const { id } = useParams();
+  const [filtredTasks, setFiltredTasks] = useState([]);
 
   useEffect(() => {
     dispatch(getSprintsTasks(id));
   }, []);
+  useEffect(() => {
+    const res = tasks.filter((task) => tasks.title.includes(filter));
+    setFiltredTasks(res);
+  }, [filter]);
+
+  //   const filterTasks = () => {
+  //     const res = tasks.filter((task) => tasks.title.includes(Filter));
+  //     return res;
+  //   };
 
   return (
     <Tasklist>
