82023c2130eff631e2ec9ef2274e81cb968eaa85
Oleksandr2891 <allavrenko@gmail.com>
2021-09-21 21:59:38 +0300

    handdlerError

--- src/redux/task/task-operations.js
+++ src/redux/task/task-operations.js
@@ -1,28 +1,43 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getError } from "../error/error-handler";
 
 export const addTask = createAsyncThunk(
-
   "task/addTask",
-  async ({ sprintId, task }) => {
-
+  async ({ sprintId, task }, { dispatch, rejectWithValue }) => {
     try {
       const { data } = await axios.post(`/task/${sprintId}`, task);
       return data;
     } catch (error) {
-      console.log(error);
+      dispatch(
+        getError({
+          error,
+          cb: () => addTask(),
+          operationType: "sprint/addSprint",
+        })
+      );
+      return rejectWithValue(error.message);
+      // throw error;
     }
   }
 );
 
 export const getSprintsTasks = createAsyncThunk(
-  'task/getTAsks',
-  async (sprintId) => {
+  "task/getTAsks",
+  async (sprintId, { dispatch, rejectWithValue }) => {
     try {
       const { data } = await axios.get(`/task/${sprintId}`);
       return data;
     } catch (error) {
-      console.log(error);
+      dispatch(
+        getError({
+          error,
+          cb: () => getSprintsTasks(),
+          operationType: "task/getTAsks",
+        })
+      );
+      return rejectWithValue(error.message);
+      // throw error;
     }
   }
 );
