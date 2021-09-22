8c0aa50c730792de115380d16dd463bbfc0028c2
Oleksandr2891 <allavrenko@gmail.com>
2021-09-22 12:59:47 +0300

    add formik

--- src/Components/auth/authForm/AuthFormStyled.js
+++ src/Components/auth/authForm/AuthFormStyled.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const AuthFormStyled = styled.form`
-  margin-top: 24px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
+export const AuthFormStyled = styled.div`
+  .inputWrapper {
+    margin-top: 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   .inputForm {
     outline: none;
     border-top: none;
@@ -13,7 +14,7 @@ export const AuthFormStyled = styled.form`
     border-left: none;
     width: 280px;
     border-bottom: 1px solid rgba(24, 28, 39, 0.2);
-    background-color: transparent;
+    background: transparent;
     padding: 10px;
     font-size: 16px;
     z-index: 300;
@@ -23,7 +24,7 @@ export const AuthFormStyled = styled.form`
     :focus {
       outline: 0;
       outline-offset: 0;
-      background-color: transparent;
+      background: transparent;
     }
 
     @media screen and (min-width: 768px) {
@@ -35,6 +36,18 @@ export const AuthFormStyled = styled.form`
   .inputForm:nth-child(3) {
     -webkit-text-security: square;
     margin-top: 50px;
+  }
+  .errors {
+    text-align: center;
+    max-width: 250px;
+    margin-top: 8px;
+    color: red;
+    font-size: 10px;
+  }
+
+  .errorPassword {
+    color: red !important;
+    border-bottom: 1px solid red;
   }
   /* 
   input {
