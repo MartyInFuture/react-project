112b1b153c5923a3ae4259d9561d7b2b34d4f178
Vlad <4ornii.vlad@gmail.com>
2021-09-22 12:25:07 +0300

    Правки стилей

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,22 @@
-import React, { lazy, Suspense, useEffect } from 'react';
-import { Switch } from 'react-router';
-import { authSelectors } from '../redux/auth';
-import { useDispatch, useSelector } from 'react-redux';
-import { token } from '../redux/auth/auth-operations';
-import { getErorrMessage } from '../redux/error/error-selector';
-import PrivateRoute from './routers/PrivateRoute';
-import PublicRoute from './routers/PublickRoute';
-import Header from './header/Header';
-import MainContainer from './common/containers/mainContainer/Container';
-import GlobalStyle from '../style/GlobalStyle';
-import WrapperContainer from './common/containers/WrapperContainer/WrapperContainer';
-import { resetErrorAction } from '../redux/error/error-action';
+import React, { lazy, Suspense, useEffect } from "react";
+import { Switch } from "react-router";
+import { authSelectors } from "../redux/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { token } from "../redux/auth/auth-operations";
+import { getErorrMessage } from "../redux/error/error-selector";
+import PrivateRoute from "./routers/PrivateRoute";
+import PublicRoute from "./routers/PublickRoute";
+import Header from "./header/Header";
+import MainContainer from "./common/containers/mainContainer/Container";
+import GlobalStyle from "../style/GlobalStyle";
+import WrapperContainer from "./common/containers/WrapperContainer/WrapperContainer";
+import { resetErrorAction } from "../redux/error/error-action";
 
-const Register = lazy(() => import('../pages/register/Register'));
-const Login = lazy(() => import('../pages/login/Login'));
-const Projects = lazy(() => import('../pages/projects/Projects'));
-const Sprints = lazy(() => import('../pages/sprints/Sprint'));
-const Tasks = lazy(() => import('../pages/tasks/Tasks'));
+const Register = lazy(() => import("../pages/register/Register"));
+const Login = lazy(() => import("../pages/login/Login"));
+const Projects = lazy(() => import("../pages/projects/Projects"));
+const Sprints = lazy(() => import("../pages/sprints/Sprint"));
+const Tasks = lazy(() => import("../pages/tasks/Tasks"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -42,7 +42,7 @@ const App = () => {
           <MainContainer>
             <WrapperContainer>
               <Switch>
-                <Suspense fallback={''}>
+                <Suspense fallback={""}>
                   <PublicRoute path="/register" exact restricted>
                     <Register />
                   </PublicRoute>
