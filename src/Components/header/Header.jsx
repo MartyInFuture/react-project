26c43b3da66e52803575d336d4109f0e0b24c257
Vlad <4ornii.vlad@gmail.com>
2021-09-22 09:10:36 +0300

    Добавлено лого, сохранение юсера в локалсторэдж, подгрузка имейла в хэдер, поправлены стили левого меню десктопа

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,16 +1,22 @@
-import { HeaderStyled } from './HeaderStyled';
-import { authSelectors } from '../../redux/auth';
-import { useSelector } from 'react-redux';
+import { HeaderStyled } from "./HeaderStyled";
+import { authSelectors } from "../../redux/auth";
+import { useSelector } from "react-redux";
+import sprite from "../../image/background/symbol-defs.svg";
 
-import HeaderMenu from './headerMenu/HeaderMenu';
+import HeaderMenu from "./headerMenu/HeaderMenu";
 
 const Header = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const userEmail = useSelector(authSelectors.getUserEmail);
 
   return (
     <HeaderStyled>
-      <div className="logo">Logo</div>
-      {isLoggedIn && <HeaderMenu />}
+      <div className="logo">
+        <svg className="logo">
+          <use href={sprite + "#logo"}></use>
+        </svg>
+      </div>
+      {isLoggedIn && <HeaderMenu userEmail={userEmail} />}
     </HeaderStyled>
   );
 };
