cfc5ef64ad043e5014991f74d83955a68e07dc9a
PetiurAliona <alena.petyur@gmail.com>
2021-09-22 10:14:22 +0300

    +Loader

--- src/Components/auth/authForm/AuthForm.jsx
+++ src/Components/auth/authForm/AuthForm.jsx
@@ -1,49 +1,52 @@
-import { useState } from "react";
-import { AuthFormStyled } from "./AuthFormStyled";
-import { authOperations } from "../../../redux/auth";
-import { useDispatch } from "react-redux";
-import SubmitButton from "../../common/submitButton/SubmitButton";
+import { useState } from "react"
+import { AuthFormStyled } from "./AuthFormStyled"
+import { authOperations, authSelectors } from "../../../redux/auth"
+import { useDispatch, useSelector } from "react-redux"
+import SubmitButton from "../../common/submitButton/SubmitButton"
+import LoaderSpinner from "../../loader/Loader"
 
 const initialState = {
   email: "",
   password: "",
   repeatPassword: "",
-};
+}
 
 const AuthForm = ({ repeatPassword = true }) => {
-  const dispatch = useDispatch();
-  const [user, setUser] = useState(initialState);
+  const dispatch = useDispatch()
+  const [user, setUser] = useState(initialState)
+
+  const loading = useSelector(authSelectors.getIsLoggedIn)
 
   const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setUser((prev) => ({ ...prev, [name]: value }));
-  };
+    const { name, value } = e.target
+    setUser((prev) => ({ ...prev, [name]: value }))
+  }
 
   const onHandleSubmit = (e) => {
-    e.preventDefault();
-    const email = user.email;
-    const password = user.password;
+    e.preventDefault()
+    const email = user.email
+    const password = user.password
 
     if (repeatPassword) {
       if (user.repeatPassword === user.password) {
-        dispatch(authOperations.register({ email, password }));
+        dispatch(authOperations.register({ email, password }))
         setUser({
           email: "",
           password: "",
           repeatPassword: "",
-        });
+        })
       } else {
-        console.log(`Пароли не совпадают повторить ввод`);
+        console.log(`Пароли не совпадают повторить ввод`)
       }
     } else {
-      dispatch(authOperations.logIn({ email, password }));
+      dispatch(authOperations.logIn({ email, password }))
       setUser({
         email: "",
         password: "",
         repeatPassword: "",
-      });
+      })
     }
-  };
+  }
 
   return (
     <AuthFormStyled onSubmit={onHandleSubmit}>
@@ -76,11 +79,10 @@ const AuthForm = ({ repeatPassword = true }) => {
           required
         />
       )}
-      <SubmitButton
-        nameBtn={`${!repeatPassword ? "Увійти" : "Зареєструватись"}`}
-      />
+      <SubmitButton nameBtn={`${!repeatPassword ? "Увійти" : "Зареєструватись"}`} />
+      {loading && <LoaderSpinner />}
     </AuthFormStyled>
-  );
-};
+  )
+}
 
-export default AuthForm;
+export default AuthForm
